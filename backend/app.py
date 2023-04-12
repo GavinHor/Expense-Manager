bf8e4592db61051c2b0bea7786ec642b6c256f35
ec21362 <127890575+ec21362@users.noreply.github.com>
2023-04-12 11:56:10 +0100

    fetch data in claims list
    this commit should not be merged. It is only for demonstration
    
    in order for it to work you will need to modify package.json to include the following line at the start:
    
    "proxy": "http://localhost:5000"
    
    make sure to run both app.py and npm start

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, redirect, jsonify
 from tinydb import TinyDB, Query
 from datetime import datetime
+import os, os.path
 
 app = Flask(__name__)
-db = TinyDB('db.json') # make db if it doesn't exist
+db = TinyDB('./db.json') # make db if it doesn't exist
 claims = db.table('claims') # make table if it doesn't exist
+
+print(claims.all())
+
+# debug code
+# all_claims = db.all()
+# print(all_claims)
+
+# a = os.open("./db.json", os.O_RDONLY)
+# print(os.read(a, 30))
 
 @app.route('/submitClaim', methods=['POST'])
 def submitClaim():
@@ -26,15 +36,28 @@ def submitClaim():
     
     expenseClaim = {'type': claimType, 'id': claimId, 'amount': amount, 'submissionDate': submissionDate.strftime('%d/%m/%Y'), 'expenseDate': expenseDate, 'employee': employee, 'status': status, 'currency': currency, 'motive': motive, 'extra': extra, 'proofImage': proofImage, 'vat': vat}
     claims.insert(expenseClaim)
+    print("Inserted " + expenseClaim.__str__())
     return redirect("http://localhost:3000/expenseClaimInfo")
 
-@app.route('/loadClaim')
+@app.route('/loadClaim', methods=['GET'])
 def loadClaim():
     # Retrieve claim data from database
     Claim = Query()
-    claims = db.search(Claim.id.exists())
+    claimsList = db.search(Claim.id == "TC013AB")
 
-    return jsonify(claims)
+    # print(claimsList)
+
+    response = jsonify(claims.all())
+    response.headers.add('Access-Control-Allow-Origin', '*') # VERY IMPORTANT!!!
+
+    return response
+
+@app.route('/test', methods=['GET'])
+def test():
+    response = jsonify("Hello")
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
